1e8f4fe375bbdcf8a6eaaad51e7b82f481e0ceda
Faustino Zanetto <faustizanetto@gmail.com>
2021-02-26 11:07:30 -0300

    SEO integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { theme } from '../styles';
 import { Layout } from '../layouts';
+import { SEO } from '../components/seo';
 
 const App = ({ Component, pageProps }: any) => {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
         <CSSReset />
+        <SEO />
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
---
+++ utils/constants.ts
@@ -0,0 +1,7 @@
+export const SEO_TITLE = 'Portfolio';
+export const SEO_DESCRIPTION = 'Faustino Zanetto´s Personal Portfolio';
+export const SEO_KEYWORDS =
+  'portfolio, personal portfolio, faustino zanetto, faustino, programming';
+export const SEO_OG_IMAGE = 'https://chakra-templates.dev/favicon.png';
+export const SEO_TWITTER_IMAGE = 'https://chakra-templates.dev/favicon.png';
+export const PRODUCTION_URL = 'https://portfolio-retrosen.vercel.app/';
